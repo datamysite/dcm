f8eed1fdeffa2e1142b96a841aaed0de80dd7a0e
Waseem <waseem@datamysite.com>
2024-03-05 16:18:45 +0400

    retailer flow

--- resources/views/web/includes/style.blade.php
+++ resources/views/web/includes/style.blade.php
@@ -17,6 +17,6 @@
 <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
 
 <!-- Icons -->
-<link rel="stylesheet" href="{{URL::to('/public')}}/web_assets/css/all.min.css" />
-<link rel="stylesheet" href="{{URL::to('/public')}}/web_assets/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="{{URL::to('/public')}}/web_assets/css/style.css" rel="stylesheet" />
