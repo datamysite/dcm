65a29fb4b9c8bc8895d567be5650217fca34ceff
Waseem <waseem@datamysite.com>
2024-03-05 14:37:59 +0400

    retailer flow

--- app/Http/Controllers/web/RegionController.php
+++ app/Http/Controllers/web/RegionController.php
@@ -4,18 +4,38 @@ namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\States;
+use GuzzleHttp\Client;
 use Config;
 
 class RegionController extends Controller
 {
     public function index(){
 
+
+        $userIp = RegionController::getIPAddress();
+        $client = new Client();
+        $response = $client->get("https://ipinfo.io/{$userIp}?token=91b28de1f957f7");
+        $data = json_decode($response->getBody());
+
+        $states = array('Dubai', 'Abu Dhabi', 'Sharjah', 'Ajman', 'Ras Al-Khaimah', 'Fujairah', 'Umm Al-Quwain');
+
         if(!isset($_SESSION['region'])){
             session_start();
+        }
+        if(!empty($data->region) && in_array($data->region, $states)){
+            $st = States::where('name', $data->region)->first();
+            if(!empty($st->id)){
+                $region = $st->slug;
+                $_SESSION['region'] = $st->slug;
+            }else{
+                $region = 'dubai';
+                $_SESSION['region'] = 'dubai';
+            }
+        }else{
+            $region = 'dubai';
             $_SESSION['region'] = 'dubai';
         }
-
-        $region = $_SESSION['region'];
 
         return redirect()->intended('/'.$region);
     }
@@ -32,4 +52,17 @@ class RegionController extends Controller
 
         return redirect()->intended('/'.$region);
     }
+
+
+
+    public function getIPAddress() {   
+        if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+                    $ip = $_SERVER['HTTP_CLIENT_IP'];  
+        }elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+        }else{  
+                 $ip = $_SERVER['REMOTE_ADDR'];  
+        }  
+        return $ip;  
+    } 
 }
