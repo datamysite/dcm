3eda40a842a249284a470d75a46a96b50b809a87
Waseem <waseem@datamysite.com>
2024-03-07 12:22:44 +0400

    arabic

--- resources/views/web/emailers/lead_welcome.blade.php
+++ resources/views/web/emailers/lead_welcome.blade.php
@@ -605,40 +605,10 @@
 
                 <div class="row">
 
-                    <div class="col-12 text-center">
-                        <div class="col-lg-12  flex-wrap">
-                            <button class="btn btn-primary px-5 py-1 mb-3 me-5 shadow-gray" style="font-weight: lighter; background-color:#00C5FF;color:#fff;">Learn More</button>
+                    <div class="col-12">
+                        <div class="col-lg-12  text-center flex-wrap">
+                            <a href="https://dealsandcouponsmena.com/" class="btn btn-primary px-5 py-1 mb-3 me-5 shadow-gray" style="font-weight: lighter; background-color:#00C5FF;color:#fff;">Learn More</a>
                         </div>
-                    </div>
-              
-                    <div class="col-md-12 text-center">
-                        <ul class="list-inline mb-0 small mt-3 mt-md-0" style="text-align:center; width: 85%;">
-
-                            <li class="list-inline-item me-1">
-                                <a href="#!" class="btn btn-xs btn-social btn-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-                                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-                                    </svg>
-                                </a>
-                            </li>
-
-                            <li class="list-inline-item me-1">
-                                <a href="#!" class="btn btn-xs btn-social btn-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 16 16">
-                                        <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-                                    </svg>
-                                </a>
-                            </li>
-
-                            <li class="list-inline-item">
-                                <a href="#!" class="btn btn-xs btn-social btn-icon">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter" viewBox="0 0 24 24">
-                                        <path d="M12.031 6.172c-3.181 0-5.767 2.586-5.768 5.766-.001 1.298.38 2.27 1.019 3.287l-.582 2.128 2.182-.573c.978.58 1.911.928 3.145.929 3.178 0 5.767-2.587 5.768-5.766.001-3.187-2.575-5.77-5.764-5.771zm3.392 8.244c-.144.405-.837.774-1.17.824-.299.045-.677.063-1.092-.069-.252-.08-.575-.187-.988-.365-1.739-.751-2.874-2.502-2.961-2.617-.087-.116-.708-.94-.708-1.793s.448-1.273.607-1.446c.159-.173.346-.217.462-.217l.332.006c.106.005.249-.04.39.298.144.347.491 1.2.534 1.287.043.087.072.188.014.304-.058.116-.087.188-.173.289l-.26.304c-.087.086-.177.18-.076.354.101.174.449.741.964 1.201.662.591 1.221.774 1.394.86s.274.072.376-.043c.101-.116.433-.506.549-.68.116-.173.231-.145.39-.087s1.011.477 1.184.564.289.13.332.202c.045.072.045.419-.1.824zm-3.423-14.416c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm.029 18.88c-1.161 0-2.305-.292-3.318-.844l-3.677.964.984-3.595c-.607-1.052-.927-2.246-.926-3.468.001-3.825 3.113-6.937 6.937-6.937 1.856.001 3.598.723 4.907 2.034 1.31 1.311 2.031 3.054 2.03 4.908-.001 3.825-3.113 6.938-6.937 6.938z" />
-                                    </svg>
-                                </a>
-                            </li>
-
-                        </ul>
                     </div>
          
                 </div>
