be9bf0d9c0f1d846e916f6c7b25209a833f7ff0d
Waseem <waseem@datamysite.com>
2024-03-07 14:53:20 +0400

    arabic

--- resources/views/web/includes/script.blade.php
+++ resources/views/web/includes/script.blade.php
@@ -4,13 +4,8 @@
 <script src="{{URL::to('/public')}}/web_assets/libs/simplebar/dist/simplebar.min.js"></script>
 <!-- Theme JS -->
 <script src="{{URL::to('/public')}}/web_assets/js/theme.min.js"></script>
-<script src="{{URL::to('/public')}}/web_assets/js/vendors/jquery.min.js"></script>
-<script src="{{URL::to('/public')}}/web_assets/js/vendors/countdown.js"></script>
 <script src="{{URL::to('/public')}}/web_assets/libs/slick-carousel/slick/slick.min.js"></script>
 <script src="{{URL::to('/public')}}/web_assets/js/vendors/slick-slider.js"></script>
-<script src="{{URL::to('/public')}}/web_assets/libs/tiny-slider/dist/min/tiny-slider.js"></script>
-<script src="{{URL::to('/public')}}/web_assets/js/vendors/tns-slider.js"></script>
-<script src="{{URL::to('/public')}}/web_assets/js/vendors/zoom.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript">
 
