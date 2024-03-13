aba251889b5c2b3f590182620f5d2246e999ccae
Waseem <waseem@datamysite.com>
2024-03-13 11:15:38 +0400

    retailer flow

--- resources/views/web/includes/script.blade.php
+++ resources/views/web/includes/script.blade.php
@@ -1,10 +1,10 @@
 <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <!-- Libs JS -->
-<script src="{{URL::to('/public')}}/web_assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js" async defer></script>
+<script src="{{URL::to('/public')}}/web_assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Theme JS -->
-<script src="{{URL::to('/public')}}/web_assets/js/theme.min.js" async defer></script>
-<script src="{{URL::to('/public')}}/web_assets/libs/slick-carousel/slick/slick.min.js" async defer></script>
-<script src="{{URL::to('/public')}}/web_assets/js/slider.js" async defer></script>
+<script src="{{URL::to('/public')}}/web_assets/js/theme.min.js"></script>
+<script src="{{URL::to('/public')}}/web_assets/libs/slick-carousel/slick/slick.min.js"></script>
+<script src="{{URL::to('/public')}}/web_assets/js/slider.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" async defer></script>
 <script type="text/javascript">
 
