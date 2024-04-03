6f94b7ae38c06f5ccced1ce5f58c0eb4b0d4b6d0
Waseem <waseem@datamysite.com>
2024-04-03 16:52:58 +0400

    amp test

--- resources/views/amp-web/includes/script.blade.php
+++ resources/views/amp-web/includes/script.blade.php
@@ -1,11 +1,11 @@
-<script async src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <!-- Libs JS -->
 <script src="{{URL::to('/public')}}/web_assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Theme JS -->
 <script src="{{URL::to('/public')}}/web_assets/js/theme.min.js"></script>
 <script src="{{URL::to('/public')}}/web_assets/libs/slick-carousel/slick/slick.min.js"></script>
 <script src="{{URL::to('/public')}}/web_assets/js/slider.js"></script>
-<script async src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript">
 
     $(document).on('keyup', '.main-search', function(){
