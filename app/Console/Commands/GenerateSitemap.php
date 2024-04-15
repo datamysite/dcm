c9a0c3cfc5513a2329afb03e5204b27a4b2a5ab1
Waseem <waseem@datamysite.com>
2024-04-15 16:34:11 +0400

    amp

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -37,11 +37,11 @@ class GenerateSitemap extends Command
             $blogSitemap = Sitemap::create();
             States::get()->each(function (States $state) use ($ensitmap, $blogSitemap) {
                 $ensitmap->add(
-                    Url::create("/en/".$state->slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                    Url::create("/en/".$state->slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                 );
 
-                $ensitmap->add(Url::create("/en/".$state->slug."/stores/online")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/en/".$state->slug."/stores/retail")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
+                $ensitmap->add(Url::create("/en/".$state->slug."/stores/online")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/en/".$state->slug."/stores/retail")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
 
                 Categories::get()->each(function (Categories $cat) use ($ensitmap, $state) {
 
@@ -51,12 +51,12 @@ class GenerateSitemap extends Command
                    $slug = preg_replace('/[^a-z0-9-]/', '', $string);
 
                     $ensitmap->add(
-                        Url::create("/en/".$state->slug."/category/".$slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                        Url::create("/en/".$state->slug."/category/".$slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                     );
 
                     if($cat->id <= 6){
-                        $ensitmap->add(Url::create("/en/".$state->slug."/category/".$slug."/online")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                        $ensitmap->add(Url::create("/en/".$state->slug."/category/".$slug."/retail")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)); 
+                        $ensitmap->add(Url::create("/en/".$state->slug."/category/".$slug."/online")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                        $ensitmap->add(Url::create("/en/".$state->slug."/category/".$slug."/retail")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)); 
                     }
 
                 });
@@ -81,17 +81,17 @@ class GenerateSitemap extends Command
 
                 });
 
-                $ensitmap->add(Url::create("/en/".$state->slug."/about-Us")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/en/".$state->slug."/sell-sith-dcm")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/en/".$state->slug."/faqs")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/en/".$state->slug."/privacy-policy")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/en/".$state->slug."/terms")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/en/".$state->slug."/anti-spam")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
+                $ensitmap->add(Url::create("/en/".$state->slug."/about-Us")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/en/".$state->slug."/sell-sith-dcm")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/en/".$state->slug."/faqs")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/en/".$state->slug."/privacy-policy")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/en/".$state->slug."/terms")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/en/".$state->slug."/anti-spam")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
 
 
                 //Blogs
 
-                $blogSitemap->add(Url::create("/en/".$state->slug."/blogs")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
+                $blogSitemap->add(Url::create("/en/".$state->slug."/blogs")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
                 Blogs::where('status', '1')->get()->each(function (Blogs $b) use ($blogSitemap, $state) {
 
                     $blogSitemap->add(
@@ -115,11 +115,11 @@ class GenerateSitemap extends Command
             $ensitmap = Sitemap::create();
             States::get()->each(function (States $state) use ($ensitmap) {
                 $ensitmap->add(
-                    Url::create("/ar/".$state->slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                    Url::create("/ar/".$state->slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                 );
 
-                $ensitmap->add(Url::create("/ar/".$state->slug."/stores/online")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/ar/".$state->slug."/stores/retail")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
+                $ensitmap->add(Url::create("/ar/".$state->slug."/stores/online")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/ar/".$state->slug."/stores/retail")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
 
                 Categories::get()->each(function (Categories $cat) use ($ensitmap, $state) {
 
@@ -129,12 +129,12 @@ class GenerateSitemap extends Command
                    $slug = preg_replace('/[^a-z0-9-]/', '', $string);
 
                     $ensitmap->add(
-                        Url::create("/ar/".$state->slug."/category/".$slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                        Url::create("/ar/".$state->slug."/category/".$slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                     );
 
                     if($cat->id <= 6){
-                        $ensitmap->add(Url::create("/ar/".$state->slug."/category/".$slug."/online")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                        $ensitmap->add(Url::create("/ar/".$state->slug."/category/".$slug."/retail")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)); 
+                        $ensitmap->add(Url::create("/ar/".$state->slug."/category/".$slug."/online")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                        $ensitmap->add(Url::create("/ar/".$state->slug."/category/".$slug."/retail")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)); 
                     }
 
                 });
@@ -142,7 +142,7 @@ class GenerateSitemap extends Command
                 Retailers::where('type', '1')->get()->each(function (Retailers $ret) use ($ensitmap, $state) {
 
                     $ensitmap->add(
-                        Url::create("/ar/".$state->slug."/store/".$ret->slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                        Url::create("/ar/".$state->slug."/store/".$ret->slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                     );
 
                 });
@@ -154,18 +154,18 @@ class GenerateSitemap extends Command
                 ->get()->each(function (Retailers $ret) use ($ensitmap, $state) {
 
                     $ensitmap->add(
-                        Url::create("/ar/".$state->slug."/store/".$ret->slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
+                        Url::create("/ar/".$state->slug."/store/".$ret->slug)->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                     );
 
                 });
 
 
-                $ensitmap->add(Url::create("/ar/".$state->slug."/about-Us")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/ar/".$state->slug."/sell-sith-dcm")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/ar/".$state->slug."/faqs")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/ar/".$state->slug."/privacy-policy")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/ar/".$state->slug."/terms")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
-                $ensitmap->add(Url::create("/ar/".$state->slug."/anti-spam")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY));
+                $ensitmap->add(Url::create("/ar/".$state->slug."/about-Us")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/ar/".$state->slug."/sell-sith-dcm")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/ar/".$state->slug."/faqs")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/ar/".$state->slug."/privacy-policy")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/ar/".$state->slug."/terms")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
+                $ensitmap->add(Url::create("/ar/".$state->slug."/anti-spam")->setPriority(1)->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY));
 
             });
             $ensitmap->writeToFile(base_path('/ar/sitemap.xml'));
