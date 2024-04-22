d6979e61ed9455aefca68f57d61e7d10c3abf706
Waseem <waseem@datamysite.com>
2024-04-22 17:41:25 +0400

    retailer flow

--- resources/views/web/includes/master.blade.php
+++ resources/views/web/includes/master.blade.php
@@ -4,7 +4,7 @@
 <head>
    <!-- Required meta tags -->
    <meta charset="utf-8" />
-   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1, user-scalable=no" />
    <meta name="home_url" content="{{route('home', [$region])}}">
    <title>{{@$metaTags->title}}</title>
    <meta name="keywords" content="{{@$metaTags->keywords}}" />
