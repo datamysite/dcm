30b1bd657e3ffdd49acb3cf43f57ad8fcb0b5447
Waseem <waseem@datamysite.com>
2024-04-23 15:53:58 +0400

    amp

--- resources/views/amp-web/includes/script.blade.php
+++ resources/views/amp-web/includes/script.blade.php
@@ -1,3 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<!-- Libs JS -->
 <script src="{{URL::to('/public')}}/web_assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Theme JS -->
 <script src="{{URL::to('/public')}}/web_assets/js/theme.min.js"></script>
