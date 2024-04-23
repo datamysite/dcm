6d4dc7158f28d15bb829374a8f8ac12a561dc691
Waseem <waseem@datamysite.com>
2024-04-23 15:37:27 +0400

    amp

--- resources/views/amp-web/includes/script.blade.php
+++ resources/views/amp-web/includes/script.blade.php
@@ -1,5 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<!-- Libs JS -->
 <script src="{{URL::to('/public')}}/web_assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Theme JS -->
 <script src="{{URL::to('/public')}}/web_assets/js/theme.min.js"></script>
