67ca43542f2edc0e953788469b2325d58d7a0df2
Waseem <waseem@datamysite.com>
2024-04-25 18:19:37 +0400

    amp

--- resources/views/ampn-web/includes/style.blade.php
+++ resources/views/ampn-web/includes/style.blade.php
@@ -1,5 +1,5 @@
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Poppins'>
-<script defer src="https://cdn.ampproject.org/v0.js"></script>
+<script async src="https://cdn.ampproject.org/v0.js"></script>
 <script defer custom-element="amp-script" src="https://cdn.ampproject.org/v0/amp-script-0.1.js"></script>
 <script defer custom-element="amp-accordion" src="https://cdn.ampproject.org/v0/amp-accordion-0.1.js"></script>
 <script defer custom-element="amp-carousel" src="https://cdn.ampproject.org/v0/amp-carousel-0.1.js"></script>
