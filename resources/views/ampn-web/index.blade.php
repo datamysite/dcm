ce735afcdafca22ecba027bf308b72752249a074
Waseem <waseem@datamysite.com>
2024-04-26 11:13:56 +0400

    amp

--- resources/views/ampn-web/index.blade.php
+++ resources/views/ampn-web/index.blade.php
@@ -127,16 +127,6 @@
                      @php $s++; if($s==3){ echo '</div>'; $i++;} if($s==3 && $i==2){ echo '<div class="brand-item">';$s=1;} @endphp
                   @endforeach
                </div>
-               @if(count($retailstores) < 2)
-                  <div class="brand-item">
-                     @for($i=0; $i<(2-count($retailstores)); $i++)
-                     <a href="#" class="img-pop-up" aria-label="Retail Stores">
-                        <amp-img class="img-fluid w-100" src="{{URL::to('/public')}}/{{app()->getLocale() == 'ar' ? 'ar-coming-soon.png' : 'coming-soon.png'}}" layout="responsive" width="185px" height="230px" alt="Store Coming Soon" style="border-radius: 20px;"></amp-img>
-                              
-                     </a>
-                     @endfor
-                  </div>
-               @endif
             </amp-carousel>
          </div>
 
