653383cb6e6424de3934fb2b45f1b23f7bb3a180
Waseem <waseem@datamysite.com>
2024-05-17 16:26:38 +0400

    retailer flow

--- app/Http/Controllers/web/HomeController.php
+++ app/Http/Controllers/web/HomeController.php
@@ -29,6 +29,7 @@ class HomeController extends Controller
         $data['onlinestores'] = DB::table('homestores')->where('retailer_type', '1')
                                 ->select('retailers.id','retailers.name','retailers.name_ar','retailers.alt_tag','retailers.alt_tag_ar', 'retailers.logo', 'retailers.ar_logo', 'retailers.slug', 'retailers.discount_upto')
                                 ->join('retailers', 'retailers.id', '=', 'homestores.retailer_id')
+                                ->where('homestores.del', '0')
                                 ->when(config('app.amp') == true && $isMobile, function($q){
                                     return $q->limit(4);
                                 })
