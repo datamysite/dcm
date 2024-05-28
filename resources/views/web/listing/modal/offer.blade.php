fa23f837774bd97c6b8a3ad71b613c8003de75de
Waseem <waseem@datamysite.com>
2024-05-28 13:19:58 +0400

    retailer flow

--- resources/views/web/listing/modal/offer.blade.php
+++ resources/views/web/listing/modal/offer.blade.php
@@ -1,1 +1,5 @@
 <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->merge('https://dealsandcouponsmena.ae/public/web_assets/images/logo/logo-DCM.png', .2, true)->size(300)->generate('https://dealsandcouponsmena.ae')) !!} ">
+
+<br><br>
+
+<img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->merge('https://dealsandcouponsmena.ae/public/web_assets/images/logo/logo-DCM.png', .2, true)->size(300)->generate('https://dealsandcouponsmena.ae/contest/dcm-extravaganza')) !!} ">
