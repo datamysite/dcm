57dad9a989b5c0d29aa133f87e9230a672ada246
Waseem <waseem@datamysite.com>
2024-05-28 17:51:35 +0400

    Google Analytics

--- resources/views/web/listing/modal/offer.blade.php
+++ resources/views/web/listing/modal/offer.blade.php
@@ -1,5 +1,3 @@
-<img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->merge('https://www.instagram.com/dealsandcouponsmena/', .2, true)->size(300)->generate('https://dealsandcouponsmena.ae')) !!} ">
 
-<br><br>
 
-<img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->merge('https://dealsandcouponsmena.ae/public/web_assets/images/logo/logo-DCM.png', .2, true)->size(300)->generate('https://dealsandcouponsmena.ae/contest/dcm-extravaganza')) !!} ">
+<img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->merge('https://dealsandcouponsmena.ae/public/web_assets/images/logo/logo-DCM.png', .2, true)->size(300)->generate('https://www.instagram.com/dealsandcouponsmena/')) !!} ">
