2202332850e8c1d95c9b60d61cd7e0070967f705
MohammedPrince <mohammed.beng@gmail.com>
2024-07-31 11:10:12 +0400

    Clickable Blogs

--- resources/views/web/blogs/categories.blade.php
+++ resources/views/web/blogs/categories.blade.php
@@ -59,12 +59,14 @@
         <div class="row" style="align-items: center;justify-content: center;">
             @foreach($data['blog'] as $val)
                 <div class="col-lg-4 blogItem mt-5">
+                <a href="{{route('blog.details', [$val->slug])}}" target="blank" style="color:#000;">
                     <div class="post-feather">
                         <img src="{{ config('app.storage').'blogs/'.$val->banner }}" alt="{{empty($val->banner_alt) ? $val->slug : $val->banner_alt}}">
                         <a href="{{route('blog.details', [$val->slug])}}" target="blank" class="readMorebutton">Read More</a>
                     </div>
                     <h5 title="{{$val->heading}}">{{$val->heading}}</h5>
                     <p title="{{ $val->short_description }}">{{ $val->short_description }}</p>
+                </a>
                 </div>
             @endforeach
 
