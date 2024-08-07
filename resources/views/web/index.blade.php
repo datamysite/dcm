80bbdeee4553382ba7dc78d5e2ccfb5b9bd375a8
Waseem <waseem@datamysite.com>
2024-08-07 12:16:39 +0400

    og tags

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -114,7 +114,7 @@
             @endphp
             <div class="item ">
                <a href="{{URL::to('/'.app()->getLocale().'/'.$slug)}}/?type={{$val->type == '3' ? '1' : '2'}}" class="text-decoration-none text-inherit">
-                  <img src="{{config('app.storage').'categories/'.$val->image}}" alt="Image - {{$val->name}}" onclick="return gtag_report_categoryview;" class="{{$slug}} category_view" width="100px" height="100px" />
+                  <img src="{{config('app.storage').'categories/'.$val->image}}" alt="Image - {{$val->name}}" onclick="return gtag_report_categoryview;" class="{{$slug}} category_view"/>
                   <div class="text-truncate">{{app()->getLocale() == 'ar' ? $val->name_ar : $val->name}}</div>
                </a>
             </div>
