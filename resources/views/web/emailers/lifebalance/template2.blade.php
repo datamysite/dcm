46e69460ba5af6a0fbc495f3cbef05f9c1e2001b
Waseem <waseem@datamysite.com>
2024-10-09 12:47:30 +0400

    og tags

--- resources/views/web/emailers/lifebalance/template2.blade.php
+++ resources/views/web/emailers/lifebalance/template2.blade.php
@@ -254,7 +254,7 @@
 
         </div>
 
-        <div class="content">
+        <div class="content" style="width:100%; overflow-y: hidden;">
 
             <div style="padding-top:20px; width:auto; text-align: left;">
 
