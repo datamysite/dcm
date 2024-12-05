4ecb90b85743312f94e9dd28a5fb8ca18075238f
Waseem <waseem@datamysite.com>
2024-12-05 16:13:08 +0400

    jetour emailer

--- routes/web.php
+++ routes/web.php
@@ -43,7 +43,7 @@ Route::get('/newsletter-mail-2', function(){
 
 Route::get('/jetour-mailer', function(){
     Mail::send('web.emailers.jetour.template1', array(), function($message) {
-        $message->to('waseem@datamysite.com', 'Waseem')->subject
+        $message->to('captain.wasi@gmail.com', 'Waseem')->subject
             ('DMS x JETOUR');
         $message->from('mail_sender2@dealsandcouponsmena.com','DCM');
     });
