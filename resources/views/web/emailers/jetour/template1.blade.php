b1f40c8dde0b671d04e053441929273051b77764
Waseem <waseem@datamysite.com>
2024-12-05 15:32:05 +0400

    jetour emailer

---
+++ resources/views/web/emailers/jetour/template1.blade.php
@@ -0,0 +1,400 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <style>
+        body {
+            font-family: poppins, Arial;
+            font-size: 18px;
+            margin-top: 0;
+        }
+
+        .container {
+            width: 60%;
+            margin: auto;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .header {
+            display: flex;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        .text-center {
+            text-align: center !important;
+        }
+
+        .logo {
+            text-align: center;
+            align-items: center;
+            height: 30%;
+            width: 30%;
+        }
+
+        .content {
+            width: 80%;
+            margin-top: 20px;
+            text-align: center;
+            justify-content: center;
+            justify-self: center;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        p {
+            margin-bottom: 1rem;
+            margin-top: 0;
+        }
+
+        .footer {
+            text-align: center;
+            width: 100%;
+        }
+
+        .social-icons a {
+            margin-left: 10px;
+            width: 25px;
+            height: 25px;
+            color: #fff;
+            line-height: 30px;
+            font-size: 18px;
+            border-radius: 50%;
+            transition: background-color 0.3s ease;
+            float: right;
+        }
+
+        .social-icons a img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: 50%;
+        }
+
+        .left-side-icons a {
+            margin-left: 10px;
+            width: 30px;
+            height: 30px;
+            color: #fff;
+            line-height: 30px;
+            font-size: 18px;
+            border-radius: 50%;
+            transition: background-color 0.3s ease;
+            float: left;
+        }
+
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+            margin: -10px;
+            width:100%
+            /* Adjust the margin value as needed */
+        }
+
+        .column {
+            flex-basis: 50%;
+            /* Adjust the flex-basis value as needed */
+            padding: 0px;
+            /* Adjust the padding value as needed */
+        }
+
+        .left {
+            float: left;
+        }
+
+        .right {
+            float: right;
+        }
+
+        .divider {
+            width: 5px;
+            height: 10px;
+        }
+
+        .dcm_banner {
+            width: 100%;
+            height: 450px;
+            background-color: beige;
+            border-radius: 20px;
+            box-shadow: 5px 5px 5px whitesmoke;
+            top: 100px;
+            opacity: 0.9;
+        }
+
+
+        .btn {
+            font-size: 14px;
+            padding: 6px 12px;
+            margin-bottom: 0;
+            border-radius: 10px;
+            display: inline-block;
+            text-decoration: none;
+            text-align: center;
+            white-space: nowrap;
+            vertical-align: middle;
+            -ms-touch-action: manipulation;
+            touch-action: manipulation;
+            cursor: pointer;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            background-image: none;
+            border: 1px solid transparent;
+        }
+
+        .btn-default {
+            color: #333;
+            background-color: #fff;
+            border-color: #ccc;
+        }
+
+        @media (max-width: 600px) {
+            .header {
+                flex-direction: column;
+            }
+
+            .container {
+                max-width: 600px;
+                margin: auto;
+                margin-top: 20px;
+            }
+
+            .container{
+                width: 100%;
+            }
+
+        }
+
+        /* 2 columns (600px) */
+        @media only screen and (min-width:600px) {
+            .container .col {
+                float: left;
+                width: 50%;
+            }
+
+        }
+
+        /* 3 columns (768px) */
+        @media only screen and (min-width:768px) {
+            .container .col {
+                width: 33.333%;
+            }
+
+        }
+
+        /* 4 columns (992px) */
+        @media only screen and (min-width:992px) {
+            .container .col {
+                width: 25%;
+            }
+
+        }
+
+         @media only screen and (max-width:992px) {
+            .container, .header {
+                width: 100%;
+            }
+
+        }
+
+        @media (max-width: 767px) {
+            /*  767px */
+
+            .dcm_banner_mobile {
+                display: block;
+                width: 100%;
+                height: 100px;
+                background-color: #37b7e5;
+                border-radius: 20px;
+                box-shadow: 5px 5px 5px whitesmoke;
+                top: 100px;
+                opacity: 0.9;
+
+            }
+
+            
+        }
+
+        .w-15 {
+            width: 15% !important;
+        }
+
+        .w-50 {
+            width: 50% !important;
+        }
+
+        .mt-5 {
+            margin-top: 3rem !important;
+        }
+
+        .dcm_banner_mobile {
+            display: none;
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <div class="container">
+
+        <div class="header">
+            <div class="dcm_banner"
+                style="background: url(https://dealsandcouponsmena.ae/public/emailers/temp11.png) no-repeat; background-size: cover; border-radius: 0; background-position: center ; height: 450px;">
+
+                <div class="row mt-5" style="text-align: center;">
+                    <div style="position: absolute;top: 130px; text-align: center;margin: auto;">
+                        <br><br>
+                        <h4 class="text-center" style="color: #fff;font-size: 30px;margin-bottom: 0 !important;">Is Stress Taking You Over? <br>Rebalance Your Life</h4>
+                        <h6 class="text-center" style="color: #fff;font-weight: normal;font-size: 17px;margin-top: 10px;">Get 10% OFF on All Products</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="content">
+
+
+                <div style="justify-content: center; width: 100%;">
+                        <br>
+                        <p style="font-size: 16px; text-align: left;">
+                            <strong>Hi,</strong>
+                            <br><br>
+                            We all need a little time to reconnect with ourselves, nurture our energy, and embrace self-love. At Life Balance, we are committed to helping you restore balance with products that bring harmony to both your body and spirit.
+
+                        <br><br><br>
+                        </p>
+                        <div class="row">
+                            <div class="column left" style="    flex-basis:65%">
+                                <h4 style="text-align:left;">About Us</h4>
+                                <p style="font-size: 16px; text-align: left;">
+                                    Life Balance is dedicated to your well-being, offering a range of holistic products designed to support your chakra system and promote overall balance. Whether you’re looking to cleanse your energy, enhance your spiritual practice, or simply take a moment to recharge, our products are here to guide you on that journey.
+                                    <br><br>
+                                    <a href="https://dealsandcouponsmena.ae/en/life-balance" target="_blank" style="text-decoration:none;padding: 5px 15px;background-color: #201c1c;color:#fff;border-radius:15px">Learn More</a>
+                                </p>
+                            </div>
+                            <div class="column right" style="    flex-basis:35%; text-align: right;">
+                                <img src="https://dealsandcouponsmena.ae/public/emailers/lifebalance.png" alt="DCM" style="width: 70%;">
+                            </div>
+                        </div>
+                        <div class="divider"></div>
+                        <hr style="height: 1px;background-color: black;border: none;margin: 10px 0;width: 100%; margin-top: 40px;"/>
+                        <div class="row">
+                            <h4 style="text-align:left;">Our Signature Products:</h4>
+                            <p style="font-size: 16px; text-align: left;">
+                                <ul style="text-align:left;">
+                                    <li><strong>Chakra Salts:</strong> Perfect for cleansing your energy field, these salts promote detoxification and chakra alignment.</li>
+                                    <li><strong>Chakra Oils:</strong> Infused with natural oils, these blends help open and balance your chakras, promoting inner peace.</li>
+                                    <li><strong>Chakra Blends:</strong> Specially formulated to harmonize and enhance the energy flow through your chakras, helping you achieve balance and tranquility.</li>
+                                    <li><strong>Foot Detox:</strong> Our foot detox products are designed to pull out toxins and revitalize your energy from the ground up, perfect for deep relaxation and rejuvenation.</li>
+                                    <li><strong>Cleanser, Balancer & Enhancer Kit:</strong> A complete solution for clearing negative energy, restoring balance, and elevating your wellness practice.</li>
+                                    <li><strong>Wellness Kits:</strong> Thoughtfully curated collections to help you relax, recharge, and maintain a sense of harmony in your daily life.</li>
+
+                                </ul>
+                            </p>
+                        </div>
+
+                        <img src="https://dealsandcouponsmena.ae/public/emailers/temp12.png" alt="DCM" style="width: 100%;">
+
+                        <div class="row">
+                            <h4 style="text-align:left;margin-top: 50px;margin-bottom: 0;">Why Choose Life Balance?</h4>
+                            <p style="font-size: 16px; text-align: left;">
+                                <ul style="text-align:left;">
+                                    <li>
+                                        <strong>100% Ayurvedic:</strong> 
+                                        Our products are crafted using traditional Ayurvedic principles, harnessing the healing power of ancient wisdom for modern wellness.
+                                    </li>
+                                    <li>
+                                        <strong>Plant-Based:</strong> 
+                                        We use only the finest plant-based ingredients, ensuring that each product is natural, effective, and gentle on your body.
+                                    </li>
+                                    <li>
+                                        <strong>Cruelty-Free:</strong> 
+                                        We are committed to ethical practices, and our products are never tested on animals, reflecting our dedication to compassionate care.
+                                    </li>
+                                    <li>
+                                        <strong>Natural Ingredients:</strong> 
+                                        We use only the highest-quality natural ingredients, sourced responsibly, to ensure that every product supports your wellness goals.
+                                    </li>
+                                    <li>
+                                        <strong>Commitment to Balance:</strong> 
+                                        We don’t just focus on physical wellness—we’re here to help you create a life of balance, energy, and harmony.
+                                    </li>
+
+                                </ul>
+                            </p>
+                        </div>
+                        <img src="https://dealsandcouponsmena.ae/public/emailers/temp23.png" alt="DCM" style="width: 100%;">
+
+                        <div class="row">
+                            <p style="font-size: 16px;text-align: left;padding: 15px 0px 5px;line-height: 24px;">
+                                <br>
+                                In celebration of self-love and inner balance, we’re offering you an exclusive 10% discount on all products. Use code <strong>DCM</strong> at checkout and experience the transformative power of our products.
+                            </p>
+                        </div>
+                </div>
+
+
+
+                <!-- Footer -->
+
+            <div style="padding-top:20px; width:auto; text-align: left; width: 100%;">
+                <div class="row" style="margin-top:30px">
+
+                    <div class="column left" style="width: 50%;">
+                        <img src="https://dealsandcouponsmena.ae/public/emailers/FAFA.png" style="width:60px;margin-bottom: -50px;" alt="Instagram">
+                        <div class="left-side-icons">
+                            Help Center<br><a href="mailto:info@dealsandcouponsmena.com" target="_blank"
+                                style="color: #00c5ff; margin-left: 0;width: 100%;">info@dealsandcouponsmena.com</a>
+                        </div>
+                    </div>
+
+                    <div class="column right" style="flex-basis: 100%; width: 50%; text-align: right;">
+                        <div class="social-icons" style="margin-top: 50px;">
+                            <a href="https://www.instagram.com/dealsandcouponsmena/" target="_blank">
+                                <img src="https://dealsandcouponsmena.ae/public/web_assets/images/emails/instagram.png" alt="Instagram" height="25px" width="25px">
+                            </a>
+                            <a href="https://www.facebook.com/people/Deals-And-Coupons-Mena/100091291623092/" target="_blank">
+                                <img src="https://dealsandcouponsmena.ae/public/web_assets/images/emails/facebook.png" alt="Facebook" height="25px" width="25px">
+                            </a>
+                            <a href="https://ae.linkedin.com/company/dealsandcouponsmena-com" target="_blank">
+                                <img src="https://dealsandcouponsmena.ae/public/web_assets/images/emails/linkedin.png" alt="linkedin" height="25px" width="25px">
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="divider"></div>
+
+                <hr style="height: 1px;background-color: black;border: none;margin: 10px 0;width: 100%; margin-top: 40px;"/>
+
+                <div class="footer" style="padding-top: 10px; text-align: center;">
+
+                    <p>
+                        You are receiving this email as you’re registered on
+                        <a href="https://dealsandcouponsmena.com/en/dubai/privacy-policy" target="_blank"
+                            style="color: #00c5ff;text-decoration: none;">dealsandcouponsmena.com</a>
+                    </p>
+                    <p> <a href="https://dealsandcouponsmena.com/en/dubai/privacy-policy" target="_blank"
+                            style="color: #000; text-decoration: none;">Privacy Policy</a> | <a
+                            href="https://dealsandcouponsmena.com/en/dubai/terms" target="_blank" style="color: #000; text-decoration: none;">
+                            Terms & Conditions</a> | <a href="https://dealsandcouponsmena.com/en/dubai/faqs" target="_blank"
+                            style="color: #000; text-decoration: none;">FAQ</a> </p>
+                    <p style=" margin-top: -17px;">
+
+                        <a href="https://dealsandcouponsmena.com/en/dubai/privacy-policy" target="_blank"
+                            style="color: #000; text-decoration: none;">©
+                            Dealsandcouponsmena</a>
+                    </p>
+
+                </div>
+            </div>
+    </div>
+</body>
+
+</html>
